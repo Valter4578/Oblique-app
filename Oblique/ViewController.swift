44fdd2a5ce67bbf27358fc90d71add8e39e9963c
Valter4578 <mrvalter566@gmail.com>
2020-10-15 20:15:03 +0700

    Initial Commit

---
+++ Oblique/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oblique
+//
+//  Created by Максим Алексеев on 15.10.2020.
+//  Copyright © 2020 Максим Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
